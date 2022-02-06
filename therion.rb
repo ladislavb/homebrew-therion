d436e2fbeb4a97cd4a7154d1034b51c6eb06a01f
ladislavb <ladislav.blazek@icloud.com>
2022-02-07 00:59:55 +0100

    brew audit therion fixes

--- therion.rb
+++ therion.rb
@@ -1,25 +1,23 @@
 class Therion < Formula
   desc "Processes survey data and generates maps or 3D models of caves"
   homepage "https://therion.speleo.sk"
-  revision 1
-
-  url "https://github.com/therion/therion.git", :revision => "f8a57f4"
+  url "https://github.com/therion/therion.git", revision: "f8a57f4"
   version "6.0.4+f8a57f4"
-
+  revision 1
   head "https://github.com/therion/therion.git"
-  
+
   depends_on "cmake" => :build
-  depends_on "pkg-config"
   depends_on "freetype"
+  depends_on "ghostscript"
   depends_on "imagemagick"
   depends_on "lcdf-typetools"
-  depends_on "proj"
-  depends_on "vtk"
-  depends_on "ghostscript"
-  depends_on "libpng"
-  depends_on "wxwidgets"
-  depends_on "tcl-tk"
   depends_on "libjpeg"
+  depends_on "libpng"
+  depends_on "pkg-config"
+  depends_on "proj"
+  depends_on "tcl-tk"
+  depends_on "vtk"
+  depends_on "wxwidgets"
   depends_on "zlib"
 
   def install
