5c5e0459825e801a1ce0ca4713ddccfb42139676
ladislavb <ladislav.blazek@icloud.com>
2022-03-08 00:10:58 +0100

    Bump to version 6.0.5+acbf071
    This version include fix for Loch on HiDPi screens

--- therion.rb
+++ therion.rb
@@ -1,8 +1,8 @@
 class Therion < Formula
   desc "Processes survey data and generates maps or 3D models of caves"
   homepage "https://therion.speleo.sk"
-  url "https://github.com/therion/therion.git", revision: "f8a57f4"
-  version "6.0.4+f8a57f4"
+  url "https://github.com/therion/therion.git", revision: "acbf071"
+  version "6.0.5+acbf071"
   revision 1
   head "https://github.com/therion/therion.git"
 
